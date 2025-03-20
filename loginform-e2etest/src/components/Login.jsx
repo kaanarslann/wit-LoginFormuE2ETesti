17dc65fdb26e4dae8f3dbf0754bf7bafe323be22
kaanarslann <kaanarslan_hp@hotmail.com>
2025-03-20 16:01:03 +0300

    login & success sayfaları

---
+++ loginform-e2etest/src/components/Login.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 8 characters long, have an uppercase letter, a lowercase letter, a number and a special character',
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false,
+    terms: false,
+  });
+  const navigate = useNavigate();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{8,}$/;
+
+
+  const handleChange = (event) => {
+    let { name, value, type } = event.target;
+    value = type === 'checkbox' ? event.target.checked : value;
+    setForm({ ...form, [name]: value });
+
+    if (name == 'email') {
+      if (emailRegex.test(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+    if (name == 'password') {
+      if (passwordRegex.test(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+    if (name == 'terms') {
+      setErrors({ ...errors, terms: !value });
+    }
+  };
+
+  useEffect(() => {
+    if (
+      emailRegex.test(form.email) &&
+      passwordRegex.test(form.password) &&
+      form.terms
+    ) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [form]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isValid) {
+      navigate('/success');
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={handleChange}
+          value={form.email}
+          invalid={errors.email}
+        />
+        {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+          invalid={errors.password}
+        />
+        {errors.password && (
+          <FormFeedback>{errorMessages.password}</FormFeedback>
+        )}
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          id="terms"
+          name="terms"
+          checked={form.terms}
+          type="checkbox"
+          disabled={!emailRegex.test(form.email) || form.password.length < 4}
+          invalid={errors.terms}
+          onChange={handleChange}
+        />{' '}
+        <Label htmlFor="terms" check>
+          I agree to terms of service and privacy policy
+        </Label>
+      </FormGroup>
+      <FormGroup className="text-center p-4">
+        <Button color="primary" disabled={!isValid}>
+          Sign In
+        </Button>
+      </FormGroup>
+    </Form>
+  );
+}
